56c354f7bcbd8ea106ae728970d47e9d8766dda4
Thansex000 <loclop3@gmail.com>
2024-02-20 03:02:56 +0700

    first commit

--- app/myApp.js
+++ app/myApp.js
@@ -172,8 +172,8 @@ app.directive('rePass', function () {
 
 
 // git init
-// git add README.md
-// git commit -m "first commit"
+// git add .  /1
+// git commit -m "first commit"//2
 // git branch -M main
 // git remote add origin https://github.com/thansex000/web207.git
-// git push -u origin main
+// git push -u origin main //3
